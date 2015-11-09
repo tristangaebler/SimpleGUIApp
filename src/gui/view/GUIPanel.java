e3a591ecb338168f1f1c44c5d972e8017187cfa6
tristangaebler <tgae6529@740s-pl-115.local>
2015-11-09 09:54:37 -0700

    Reorganized the button and added the font to comic sans

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,30 +3,47 @@ package gui.view;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 import gui.controller.GUIController;
+import java.awt.Font;
 
 public class GUIPanel extends JPanel
 {
 	private GUIController baseController;
 	private JButton firstButton;
-	private JTextField firstField;
+	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIController baseController)
 	{
+		
 		this.baseController = baseController;
+		
+		baseLayout = new SpringLayout();
+		firstButton = new JButton("If you click the button a ghost will come out of your computer");
+		firstTextField = new JTextField("Type NOW!!!");
 		setUpPanel();
 		setUpLayout();
 		setUpListeners();
 	}
 	
+	/**
+	 * Helper method to load all the GUI components into the panel
+	 */
 	private void setUpPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
 	}
 	
 	private void setUpLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -3, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 17, SpringLayout.SOUTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 169, SpringLayout.WEST, this);
+		firstButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 	}
 	
 	private void setUpListeners()
