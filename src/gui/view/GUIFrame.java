941437f0b859996aa658e7182e08ab4fb89e1f9c
tristangaebler <tgae6529@740s-pl-115.local>
2015-11-09 09:25:53 -0700

    Added the getter

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -2,22 +2,34 @@ package gui.view;
 
 import javax.swing.JFrame;
 import gui.controller.GUIController;
-
+/**
+ * 
+ * @author Tristan Gaebler
+ * @version 0.2 Nov 9,2015 updated the setUpFrame to load the contentPanel with a GUIPanel
+ */
 public class GUIFrame extends JFrame
 {
 	private GUIController baseController;
+	private GUIPanel basePanel;
 	
 	public GUIFrame(GUIController baseController)
 	{
 		this.baseController = baseController;
+		basePanel = new GUIPanel(baseController);
 		setUpFrame();
 	}
 	
 	private void setUpFrame()
 	{
-		this.setSize(400,400);
+		this.setContentPane(basePanel); //Must be the first line of the method
+		this.setSize(400,400); //Window size
 		this.setResizable(false);
-		this.setVisible(true);
+		this.setVisible(true); //Must be last line of the method
+	}
+	
+	public GUIController getBaseController()
+	{
+		return baseController;
 	}
 
 }
